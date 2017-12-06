5da85834da2e6653d857ac7c72cc921522f159d2
Bjørn Gustav Baklid <bg@dyret.net>
2017-12-06 22:31:32 +0100

    At least I finally got part1

--- Day2/Day2.ps1
+++ Day2/Day2.ps1
@@ -1,18 +1,38 @@
 #Part 1
-    $in = Get-content .\input.txt
-    [array]$in = $in.Split("`t").Split("`n").Split(" ")
- 
-    #$in = @("5195","753","2468")
-    foreach($i in $in){
-        $x = [int[]](($i -split '') -ne '')
-        $Max = ($x|Measure-Object -Maximum).Maximum
-        $Min = ($x|Measure-Object -Minimum).Minimum
+$in = Get-content .\input.txt
+[array]$in = $in.Split("`n")
+     
+$result = 0
+foreach ($i in $in) {
 
-        $Diff = $Max - $Min
+    $i = $i.Split("`t")
+  
+    $Max = ($i|Measure-Object -Maximum).Maximum
+    $Min = ($i|Measure-Object -Minimum).Minimum
 
-        $Result += $Diff
-    }
-    Write-Output "Part1:" $Result
+    $Diff = $Max - $Min
 
+    $Result += [int]$Diff
+}
+Write-Output "Part1:" $Result
 
 #Part 2
+
+foreach ($i in $in) {
+    $i = $i.Split("`t")
+    $i = foreach($n in $i){
+        [int]::Parse($n)
+    }
+
+    for($n = 0; $n -lt ($i.Count - 1); $n++){
+        if($i[$n] % $i[$n + 1] -eq 0 ){
+            $Part2Result += $i[$n] / $i[$n + 1]
+        }
+    }
+    for($n = $i.Count -1 ; $n -gt 0; $n--){
+        if($i[$n] % $i[$n - 1] -eq 0 ){
+            $Part2Result += $i[$n] / $i[$n - 1]
+        }
+    }
+}
+Write-Output "Part2: " $Part2Result
